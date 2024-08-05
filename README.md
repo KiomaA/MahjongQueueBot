3d9498d31bc4633496ccab85cecf94adb6b6ed43
KiomaA <kiomaavalon@gmail.com>
2024-08-05 21:28:27 +0800

    fetch youtube messages

---
+++ README.md
@@ -0,0 +1,37 @@
+# 友人場排隊Bot - Mahjong Queue Bot
+## By Kioma Avalon
+
+## Install
+1. Install Node.js (v20.16.0) from https://nodejs.org/
+2. Open cmd in MahjongQueue folder, enter "npm install"
+3. Change credentials_example.json to credentials.json and enter your twitch credentials (Get OAuth token here: https://twitchapps.com/tmi/)
+
+## Run
+Click "start.bat" to run server
+Alternatively, enter "node bot.js" in cmd
+
+## Config
+``
+{
+"httpPort":3001,
+"useTwitch":true,
+"useYoutube":true,
+"botName":"(Bot name)",
+"admins":["twitch admin usernames"],
+"twitchChannels":["twitch channel username for bot to chat"],
+"youtubeAdmins":["youtube admin channel ids"],
+"youtubeChannelId":"youtube channel for fetch chat",
+"useTwitchForYoutubeMessage": (boolean, whether send message to twitch after fetching youtube messages),
+}
+``
+
+## Commands
+- !mah.start                Enable Mahjong queue, queue viewers when +1 is included in chat messages
+- !mah.end                  Disable Mahjong queue
+- !mah.add (names)          Add names to queue, separated by space
+- !mah.addfront (names)     Add names to front of queue, separated by space
+- !mah.remove (names)       Remove names from queue, separated by space
+- !mah.removeidx (integers) Remove nth names from queue, starting from 0
+- !mah.3ma                  Play 3-player mahjong game, remove 2 players from queue
+- !mah.4ma                  Play 4-player mahjong game, remove 3 players from queue
+- !mah.5ma                  5ma
--- bot.js
+++ bot.js
@@ -4,19 +4,31 @@ const app = express();
 app.engine('html', require('ejs').renderFile);
 const credentials = require('./credentials.json');
 const config = require('./config.json')
+const { LiveChat } = require("youtube-chat")
+const crypto = require("crypto");
+const { channel } = require('diagnostics_channel');
 
 
-const {useTwitch,useYoutube,botName,admins,gmEnabled,bottomEnabled,magicGirlEnabled,httpPort} = config
+
+const {useTwitch,useYoutube,youtubeChannelId,useTwitchForYoutubeMessage,botName,admins,youtubeAdmins,httpPort,twitchChannels} = config
 let mahjongStart = false
 let mahjongQueue = []
 let gmed = []
 let bottoms = []
 let magicGirls = []
 
+let gmEnabled = config.gmEnabled;
+let bottomEnabled = config.bottomEnabled;
+let magicGirlEnabled =  config.magicGirlEnabled;
+
+let message = ""
+let messageId = ""
+let twitchChannel = twitchChannels[0]
+
 
 // http request queue
 app.get('/mah/queue', (req, res) => {
-  return res.json(mahjongQueue)
+  return res.json({queue:mahjongQueue, message:message, messageId:messageId})
 })
 
 app.get('/mah/overlay', function(req, res){
@@ -25,15 +37,22 @@ app.get('/mah/overlay', function(req, res){
 
 app.listen(httpPort, () => {
   console.log(`HTTP server listening on port ${httpPort}`)
+  console.log(`GET queue JSON: http://localhost:3001/mah/queue`)
+  console.log(`HTML/OBS overlay: http://localhost:3001/mah/overlay`)
 })
 
 
-
+// send message
+function sendMessage(mes,useTwitch,twitchClient,twitchChannel){
+  message = mes;
+  messageId = crypto.randomUUID();
+  
+  if (useTwitch){
+    twitchClient.say(twitchChannel, `(${botName}) `+message);
+  }
+}
 
 // tmi client
-
-if (useTwitch){
-
 const client = new tmi.Client({
   options: { debug: true },
   connection: {
@@ -41,33 +60,32 @@ const client = new tmi.Client({
     secure: true,
   },
   identity: credentials,
-  channels: ['kioma_avalon'], // 想加入的聊天室
+  channels: twitchChannels, // 想加入的聊天室
 });
 
+if (useTwitch){
 client.connect();
 client.on('message', (channel, tags, message, self) => {
     // console.log(channel,tags,message,self)
   if (self) return;
-
-
- // messages
- if (gmEnabled && message.toLowerCase().includes('早早') | message.toLowerCase().includes('早晨') ) {
-  console.log(gmed)
-  if (!gmed.includes(tags['display-name'])){
-    client.say(channel, `(${botName}) 早早呀 @${tags['display-name']} kiomaaHappy `);
-    gmed.push(tags['display-name'])
+   // messages
+   if (gmEnabled && message.toLowerCase().includes('早早') | message.toLowerCase().includes('早晨') ) {
+    console.log(gmed)
+    if (!gmed.includes(tags['display-name'])){
+      client.say(channel, `(${botName}) 早早呀 @${tags['display-name']} kiomaaHappy `);
+      gmed.push(tags['display-name'])
+    }
+  } else if (gmEnabled && message.toLowerCase().includes('good morning')) {
+    if (!gmed.includes(tags['display-name'])){
+      client.say(channel, `(${botName}) Good Morning @${tags['display-name']} kiomaaHappy `);
+      gmed.push(tags['display-name'])
+    }
+  } else if (gmEnabled && message.toLowerCase().includes('おはよ')) {
+    if (!gmed.includes(tags['display-name'])){
+      client.say(channel, `(${botName}) おはよう @${tags['display-name']} kiomaaHappy `);
+      gmed.push(tags['display-name'])
+    }
   }
-} else if (gmEnabled && message.toLowerCase().includes('good morning')) {
-  if (!gmed.includes(tags['display-name'])){
-    client.say(channel, `(${botName}) Good Morning @${tags['display-name']} kiomaaHappy `);
-    gmed.push(tags['display-name'])
-  }
-} else if (gmEnabled && message.toLowerCase().includes('おはよ')) {
-  if (!gmed.includes(tags['display-name'])){
-    client.say(channel, `(${botName}) おはよう @${tags['display-name']} kiomaaHappy `);
-    gmed.push(tags['display-name'])
-  }
-}
 
 
  if (bottomEnabled && message.toLowerCase().includes('總受') && !admins.includes(tags.username)) {
@@ -134,13 +152,15 @@ client.on('message', (channel, tags, message, self) => {
   if (message.toLowerCase()  == '!mah.start' && admins.includes(tags.username)){
     mahjongQueue = [];
     mahjongStart = true
-    client.say(channel, `(${botName}) 已啟動排隊，請留言「+1」進入友人場！ Queue Enabled. Please type "+1" at chat to queue!`);
+    sendMessage(`已啟動排隊，請留言「+1」進入友人場！ Queue Enabled. Please type "+1" at chat to queue!`,true,client,channel);
+    // client.say(channel, `(${botName}) 已啟動排隊，請留言「+1」進入友人場！ Queue Enabled. Please type "+1" at chat to queue!`)}
   }
 
   // end queue
   if (message.toLowerCase()  == '!mah.end' && admins.includes(tags.username)){
     mahjongStart = false
-    client.say(channel, `(${botName}) 已關閉排隊`);
+    sendMessage(`已關閉排隊 Queue Disabled`, true, client, channel)
+    // client.say(channel, `(${botName}) 已關閉排隊`);
   }
 
   // check queue status
@@ -155,7 +175,7 @@ client.on('message', (channel, tags, message, self) => {
     }else{
         let p1 = mahjongQueue.shift()
         let p2 = mahjongQueue.shift()
-        client.say(channel, `(${botName}) ${p1}和${p2}，請進入友人場`);
+        sendMessage(`${p1}和${p2}，請進入友人場`, true, client, channel)
     }
   }
 
@@ -167,7 +187,8 @@ client.on('message', (channel, tags, message, self) => {
         let p1 = mahjongQueue.shift()
         let p2 = mahjongQueue.shift()
         let p3 = mahjongQueue.shift()
-        client.say(channel, `(${botName}) ${p1}、${p2}和${p3}，請進入友人場`);
+        // client.say(channel, `(${botName}) ${p1}、${p2}和${p3}，請進入友人場`);
+        sendMessage(`${p1}、${p2}和${p3}，請進入友人場`, true, client, channel)
     }
   }
 
@@ -187,6 +208,7 @@ client.on('message', (channel, tags, message, self) => {
     let addNames = message.split(" ")
     addNames.shift()
     mahjongQueue = [...mahjongQueue, ...addNames]
+    sendMessage(`歡迎 ${addNames.join("、")}加入！`, false);
     client.say(channel, `(${botName}) 歡迎 ${addNames.join("、")}加入！ 目前已排： ${mahjongQueue.join(" ")}`);
   }
 
@@ -195,6 +217,7 @@ client.on('message', (channel, tags, message, self) => {
     let addNames = message.split(" ")
     addNames.shift()
     mahjongQueue = [...addNames, ...mahjongQueue]
+    sendMessage(`歡迎 ${addNames.join("、")}加入！`, false);
     client.say(channel, `(${botName}) 歡迎 ${addNames.join("、")}加入！ 目前已排： ${mahjongQueue.join(" ")}`);
   }
 
@@ -226,4 +249,135 @@ client.on('message', (channel, tags, message, self) => {
   }
 });
 }
+
+const fetchYoutube = async (useTwitch, client)=>{
+  const liveChat = new LiveChat({channelId: youtubeChannelId});
+
+  const ok = await liveChat.start()
+
+  const fetchTime = new Date()
+
+  liveChat.on("chat", (chatItem) => {
+    // process new messages only
+    if (chatItem.timestamp >= fetchTime){
+      // console.log("new message")
+      // console.log(chatItem.message[0].text)
+      // console.log(chatItem.author.name)
+      // console.log(chatItem.author.channelId)
+      // console.log(chatItem.timestamp)
+      
+      let message = chatItem.message[0].text
+      let username = chatItem.author.name
+      let channelId = chatItem.author.channelId
+
+
+      // queue +1
+      if (mahjongStart && message.toLowerCase().includes('+1')) {
+        if (!mahjongQueue.includes(username)){
+            mahjongQueue = [...mahjongQueue, username]
+            sendMessage(`歡迎 ${username} 加入！`,true,client,twitchChannel);
+        }
+      }
+
+    // admin command
+      if (message.toLowerCase()  == '!mah.start' && youtubeAdmins.includes(channelId)){
+        mahjongQueue = [];
+        mahjongStart = true
+        sendMessage(`已啟動排隊，請留言「+1」進入友人場！ Queue Enabled. Please type "+1" at chat to queue!`,true,client,twitchChannel);
+      }
+
+    // end queue
+    if (message.toLowerCase()  == '!mah.end' && youtubeAdmins.includes(channelId)){
+      mahjongStart = false
+      sendMessage(`已關閉排隊 Queue Disabled`, true, client, twitchChannel);
+    }
+
+    // 3ma
+  if (message.toLowerCase() == '!mah.3ma' && youtubeAdmins.includes(channelId)){
+    if (mahjongQueue.length < 2){
+      if (useTwitch) client.say(twitchChannel, `(${botName}) 不夠人玩啊，目前有 ${mahjongQueue.join(" ")}`);
+    }else{
+        let p1 = mahjongQueue.shift()
+        let p2 = mahjongQueue.shift()
+        sendMessage(`${p1}和${p2}，請進入友人場`, true, client, twitchChannel);
+    }
+  }
+
+  // 4ma
+  if (message.toLowerCase() == '!mah.4ma' && youtubeAdmins.includes(channelId)){
+    if (mahjongQueue.length < 3){
+      if (useTwitch) client.say(twitchChannel, `(${botName}) 不夠人玩啊，目前有 ${mahjongQueue.join(" ")}`);
+    }else{
+        let p1 = mahjongQueue.shift()
+        let p2 = mahjongQueue.shift()
+        let p3 = mahjongQueue.shift()
+        // client.say(channel, `(${botName}) ${p1}、${p2}和${p3}，請進入友人場`);
+        sendMessage(`${p1}、${p2}和${p3}，請進入友人場`, true, client, twitchChannel);
+    }
+  }
+
+  // 5ma
+  if (message.toLowerCase() == '!mah.5ma' && youtubeAdmins.includes(channelId)){
+    client.say(twitchChannel, `(${botName}) 對唔住喎日麻冇五麻呢樣嘢，最多我咪俾個5ma你囉 <3 `);
+  }
+
+  // clear queue
+  if (message.toLowerCase() == '!mah.clear' && youtubeAdmins.includes(channelId)){
+    mahjongQueue = [];
+    client.say(twitchChannel, `(${botName}) 已清除排隊`);
+  }
+
+  // add players to queue
+  if (message.toLowerCase().includes('!mah.add ') && youtubeAdmins.includes(channelId)){
+    let addNames = message.split(" ")
+    addNames.shift()
+    mahjongQueue = [...mahjongQueue, ...addNames]
+    sendMessage(`歡迎 ${addNames.join("、")}加入！`, false);
+    client.say(twitchChannel, `(${botName}) 歡迎 ${addNames.join("、")}加入！ 目前已排： ${mahjongQueue.join(" ")}`);
+  }
+
+  // add players to front of queue
+  if (message.toLowerCase().includes('!mah.addfront ') && youtubeAdmins.includes(channelId)){
+    let addNames = message.split(" ")
+    addNames.shift()
+    mahjongQueue = [...addNames, ...mahjongQueue]
+    sendMessage(`歡迎 ${addNames.join("、")}加入！`, false);
+    client.say(twitchChannel, `(${botName}) 歡迎 ${addNames.join("、")}加入！ 目前已排： ${mahjongQueue.join(" ")}`);
+  }
+
+   // remove player by name
+  if (message.toLowerCase().includes('!mah.remove ') && youtubeAdmins.includes(channelId)){
+    let removeNames = message.split(" ")
+    removeNames.shift()
+
+    removeNames.forEach(removeName => {
+      let index = mahjongQueue.indexOf(removeName);
+      if (index !== -1) {
+          mahjongQueue.splice(index, 1);
+      }
+    });
+    client.say(twitchChannel, `(${botName}) 已移除參加者，目前已排： ${mahjongQueue.join(" ")}`);
+  }
+
+  // remove nth player
+  if (message.toLowerCase().includes('!mah.removeidx ') && youtubeAdmins.includes(channelId)){
+    let removeIndexes = message.split(" ")
+    removeIndexes.shift()
+    removeIndexes.forEach(removeIndex => {
+      let index = Number.parseInt(removeIndex)
+      if (!Number.isNaN(index)) {
+          mahjongQueue.splice(index, 1);
+      }
+    });
+    client.say(twitchChannel, `(${botName}) 已移除參加者，目前已排： ${mahjongQueue.join(" ")}`);
+  }      
+
+    }
+    
+  })
+}
+
+fetchYoutube(useTwitchForYoutubeMessage,client);
+
+
 
